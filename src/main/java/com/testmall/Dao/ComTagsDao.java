ae9decf4eff746031a081be78702774cd326bfd7
Vicki0314 <iamvickirocknroll@gmail.com>
2024-02-01 18:06:31 +0800

    Changes

--- src/main/java/com/testmall/Dao/ComTagsDao.java
+++ src/main/java/com/testmall/Dao/ComTagsDao.java
@@ -15,7 +15,7 @@ public class ComTagsDao {
 
     CharsetTool cstool = new CharsetTool();
 
-    //查詢小類+大類(進DB call小類/大類,回傳data,確認影響行數不<=0)
+    //查詢小類及大類(進DB call小類/大類,回傳data,確認影響行數不<=0)
     public List<CommodityTags> queryAll(){
         String sql = "SELECT DISTINCT CommoditySubTag, CommodityMainTag FROM commodity_tags;";
 
@@ -30,12 +30,12 @@ public class ComTagsDao {
         return null;
     }
 
-    //新增小類(確認要新增的為空,新增小類,確認有改或不為空)
-    public String addCommoditySubTag(CommodityTags SubTag) {
+    //新增小類及大類(確認要新增的為空,新增,確認有改或不為空)
+    public String addCommodityTag(CommodityTags Tag) {
         try {
-            String sql ="INSERT INTO commodity_tags(CommoditySubTag) VALUES (?)";
+            String sql ="INSERT INTO commodity_tags(CommodityTag) VALUES (?)";
             int rowsAffected = jt.update(sql,
-                    cstool.utf82iso(SubTag.getCommoditySubTag()));
+                    cstool.utf82iso(Tag.getCommoditySubTag()));
             return "新增" + rowsAffected + "筆資料!";
         }
         catch (Exception e){
@@ -46,10 +46,16 @@ public class ComTagsDao {
             第二個參數 "ComTagsDao.addCommoditySubTag" 則是用來標識這條日誌訊息的相關程式碼位置或行為。*/
         }
     }
-    //新增大類
-    //刪除小類(確認有該小類,刪除小類,確認為空)
+
+
+    //刪除小類及大類(確認有該小類或大類,刪除,確認為空)
     // delete from commodity_tags where CommoditySubTag = " "
-    //刪除大類
-    //更新小類(找到指定小類,更新資料,確認影響行數不<=0)
-    //更新大類
+    public String deleteCommodityTag(List<Long> idList) {//idList要改
+
+    }
+    //更新大類(找到指定大類,更新資料,確認影響行數不<=0)
+    public String updateCommodityTag(CommodityTags commodityTags) {
+
+
+    }
 }
--- src/main/java/com/testmall/Service/ComTagsService.java
+++ src/main/java/com/testmall/Service/ComTagsService.java
@@ -1,7 +1,9 @@
 package com.testmall.Service;
 
 import com.testmall.Dao.ComTagsDao;
-import com.testmall.Model.Carts;
+import com.testmall.Dao.CommodityDao;
+import com.testmall.Model.Commodities;
+import com.testmall.Model.CommodityTags;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -11,20 +13,22 @@ import java.util.List;
 public class ComTagsService {
     @Autowired
     //↓[private=工具]包含一個私有成員變數 comTagsDao，用於處理商品類別相關的資料庫訪問操作。
-    private ComTagsDao comTagsDao;
+    ComTagsDao comTagsDao;
     //查詢
-    public List<Carts> queryAll(){
-        try{
-            return ComTagsDao.queryAll();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return null;
+    public List<CommodityTags> queryAll(){
+        return comTagsDao.queryAll();
     }
     //新增
-
+    public String addCommodityTag(CommodityTags commodityTags){
+        return comTagsDao.addCommodityTag(commodityTags);
+    }
     //刪除
+    public String deleteCommodityTag(List<Long> idList){//idList要改
+        return comTagsDao.deleteCommodityTag(idList);
+    }
     //更新(=)
+    public String updateCommodityTag(CommodityTags commodityTags){
+        return comTagsDao.updateCommodityTag(commodityTags);
+    }
 
 }
