d1e2a48468e74a052823974f287ab25bf59fec80
ms2499 <ms06240399@gmail.com>
2024-03-07 16:06:05 +0800

    補上訂單API說明

--- src/main/java/com/testmall/Controller/OrderController.java
+++ src/main/java/com/testmall/Controller/OrderController.java
@@ -5,6 +5,7 @@ import com.testmall.Model.UserOrders;
 import com.testmall.Model.OrderLists;
 import com.testmall.Service.UserOrdersService;
 import com.testmall.Service.OrderListsService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
@@ -23,36 +24,42 @@ public class OrderController {
 
     @GetMapping("/getAll")
     @ResponseBody
+    @Operation(summary = "取得所有訂單資料")
     public List<UserOrders> getOrderAll(){
         return orderService.queryAllOrders();
     }
 
     @GetMapping("/getByNo")
     @ResponseBody
+    @Operation(summary = "查詢使用者訂單資料(By 訂單編號)")
     public UserOrders getOrderByNo(int no){
         return orderService.queryOrdersByNo(no);
     }
 
     @GetMapping("getByListNo")
     @ResponseBody
+    @Operation(summary = "查詢訂單詳細資料(By 訂單編號)")
     public List<OrderLists> getListByNo(int no){
         return listService.queryListsByNo(no);
     }
 
     @PostMapping("/insertOrder")
     @ResponseBody
+    @Operation(summary = "新增訂單")
     public String insertOrder(@RequestBody(required = false) List<Carts> carts){
         return orderService.insertOrder(carts);
     }
 
     @PostMapping("/deleteOrder")
     @ResponseBody
+    @Operation(summary = "刪除訂單")
     public String deleteOrder(@RequestBody(required = false) List<Integer> no){
         return orderService.deleteOrder(no);
     }
 
     @PatchMapping("/updateOrder")
     @ResponseBody
+    @Operation(summary = "更新訂單")
     public String updateOrder(@RequestBody(required = false) UserOrders userorders){
         return orderService.updateOrder(userorders);
     }
