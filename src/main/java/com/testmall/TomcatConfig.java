4e228f55d6ecec61fe1b537589eb2de90b053cea
ms2499 <ms06240399@gmail.com>
2024-03-21 15:10:24 +0800

    新增https連線方式

---
+++ src/main/java/com/testmall/TomcatConfig.java
@@ -0,0 +1,20 @@
+package com.testmall;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(9090);
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+}
